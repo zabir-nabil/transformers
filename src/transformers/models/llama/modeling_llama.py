5e28acc48ce6c5472d29949384e1a4f60df07d97
Zabir Al Nazi Nabil <zabiralnazi@yahoo.com>
2023-07-10 05:13:08 +0600

    Update modeling_llama.py

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -264,7 +264,7 @@ class LlamaDecoderLayer(nn.Module):
         self.post_attention_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         
         # preference gate
-        self.gate = torch.nn.Parameter(torch.ones(1) * 0.0124623)
+        self.gate = torch.nn.Parameter(torch.zeros(1))
     
 
     def forward(
